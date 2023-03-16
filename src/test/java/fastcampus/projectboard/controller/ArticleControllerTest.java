403ef58d8c51ea5906ccd5611e9c16e73e031e15
tatum <vndtjsrl3@gmail.com>
2023-03-16 10:50:27 +0900

    #17 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ src/test/java/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,78 @@
+package fastcampus.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class) //해당 컨트롤러만 선택
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    /* 테스트 코드에서는 @Autowired 생략하지 말고 꼭 써줘야 한다 */
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트(게시판)페이지 - 정상 호출")  
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        //given
+        
+        //when
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles")); //articles 이름의 키가 있는지 검증
+        
+        //then
+    }
+    
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")  
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        //given
+        
+        //when
+        mvc.perform(get("/articles/1")) // 임의의 id로 테스트
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article")); //article 이름의 키가 있는지 검증
+        
+        //then
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles/search")) // 임의의 id로 테스트
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+        //then
+    }
+    
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles/search-hashtag")) // 임의의 id로 테스트
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+        //then
+    }
+}
