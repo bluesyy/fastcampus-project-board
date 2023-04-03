78b227a52706381eafa4cd448ba72f052c956c39
tatum <vndtjsrl3@gmail.com>
2023-04-03 21:27:51 +0900

    #17 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한번에 적용

--- src/main/java/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/fastcampus/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package fastcampus.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,8 +19,16 @@ import java.util.List;
 public class ArticleController {
 
     @GetMapping
-    public String article(ModelMap map) {
+    public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article"); //TODO: 구현할 때 여기에 실제 데이터 추가
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
     }
 }
